f70d1aadf65b158bb7222e863ca4ed1960e4690a
Hyun <ghkdgus29@naver.com>
2023-04-03 15:09:55 +0900

    feat: 클라이언트의  "/" , "/index.html" GET 요청이 오면 templates/index.html 을 렌더링하는 기능 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,14 +21,33 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+
+            String line = br.readLine();
+            String url = parseURL(line);
+
+            while (!line.equals("")) {
+                logger.debug(line);
+                line = br.readLine();
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("src/main/resources/templates" + url).toPath());
+
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private String parseURL(String startLine) {
+        String url = startLine.split(" ")[1];
+        if (url.equals("/")) {
+            url = "/index.html";
+        }
+        return url;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
