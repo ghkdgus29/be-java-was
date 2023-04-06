519f0b0c80c4df096a7a9d7f490c2e0be13250ed
Hyun <ghkdgus29@naver.com>
2023-04-06 10:53:33 +0900

    test: StartLineTest 에서 생성자에 null 을 넣는 대신, mock 정보를 집어 넣도록 변경

--- src/test/java/model/StartLineTest.java
+++ src/test/java/model/StartLineTest.java
@@ -3,20 +3,24 @@ package model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StartLineTest {
 
-    private static final String HTML_PATH = "src/main/resources/templates";
-    private static final String STYLE_PATH = "src/main/resources/static";
+    private static final String TEMPLATES_PATH = "src/main/resources/templates";
+    private static final String STATIC_PATH = "src/main/resources/static";
 
     @Test
     @DisplayName("일반 경로 url 이 생성자에 주입되면, HTML 경로를 반환하고, requestType은 html 이다.")
     void requestHtml() {
+        String mockMethod = "mock";
         String url = "/";
-        String expectedUrl = HTML_PATH + "/index.html";
+        Map<String, String> mockParam = Map.of("mock", "mock");
 
-        StartLine startLine = new StartLine(null, url, null);
+        String expectedUrl = TEMPLATES_PATH + "/index.html";
+        StartLine startLine = new StartLine(mockMethod, url, mockParam);
 
         assertEquals(expectedUrl, startLine.getPath());
         assertEquals("html", startLine.getRequestType());
@@ -25,10 +29,12 @@ class StartLineTest {
     @Test
     @DisplayName("css를 요청하는 url 이 생성자에 주입되면, CSS 경로를 반환하고 requestType는 css 이다.")
     void requestCss() {
+        String mockMethod = "mock";
         String url = "/css/bootstrap.min.css";
-        String expectedUrl = STYLE_PATH + url;
+        Map<String, String> mockParam = Map.of("mock", "mock");
 
-        StartLine startLine = new StartLine(null, url, null);
+        String expectedUrl = STATIC_PATH + url;
+        StartLine startLine = new StartLine(mockMethod, url, mockParam);
 
         assertEquals(expectedUrl, startLine.getPath());
         assertEquals("css", startLine.getRequestType());
