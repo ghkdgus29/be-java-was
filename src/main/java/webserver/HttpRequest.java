0158d9eb3de81eebbcf8066c9ec6950aae5162a7
Hyun <ghkdgus29@naver.com>
2023-04-12 11:32:03 +0900

    feat: HTTP Request 정보를 전부 분석해서 갖고 있는 HttpRequest 클래스 생성

--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -4,19 +4,69 @@ import model.StartLine;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class HttpRequest {
+
+    private static final String GET = "GET";
+    private static final String POST = "POST";
+    private static final int PARAM_NAME_IDX = 0;
+    private static final int PARAM_VALUE_IDX = 1;
+
 
     private final StartLine startLine;
     private final Map<String, String> headers;
     private final String messageBody;
 
+    private final Map<String, String> parameters;
+
     public HttpRequest(String requestLine, Map<String, String> headers, String messageBody) {
         this.startLine = new StartLine(requestLine);
         this.headers = headers;
         this.messageBody = messageBody;
+        this.parameters = setParameters();
+    }
+
+    public String getMethod() {
+        return startLine.getMethod();
+    }
+
+    public String getPath() {
+        return startLine.getPath();
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public Map<String, String> getParameters() {
+        return parameters;
+    }
+
+    private Map<String, String> setParameters() {
+        if (startLine.getMethod().equals(GET)) {
+            return startLine.getParamMap();
+        }
+
+        if (startLine.getMethod().equals(POST)) {
+            return parseParametersBy(messageBody);
+        }
+
+        return null;
+    }
+    
+    private Map<String, String> parseParametersBy(String messageBody) {
+        HashMap<String, String> parameters = new HashMap<>();
+
+        if (messageBody == null) {
+            return null;
+        }
+
+        String[] parameterPairs = messageBody.split("&");
+        for (String pair : parameterPairs) {
+            parameters.put(pair.split("=")[PARAM_NAME_IDX], pair.split("=")[PARAM_VALUE_IDX]);
+        }
+
+        return parameters;
     }
 
 
--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -1,6 +1,5 @@
 package webserver;
 
-import model.StartLine;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,58 +12,111 @@ class HttpRequestTest {
 
     private static final String PATH = "src/main/resources/templates";
 
-//    @Test
-//    @DisplayName("Request 의 start-line 에 포함된 GET 메서드를 파싱해서 리턴한다.")
-//    void parseMethod() {
-//        String startLineChunk = "GET /index.html HTTP/1.1";
-//
-//        StartLine startLine = HttpRequest.getStartLine(startLineChunk);
-//
-//        assertEquals("GET", startLine.getMethod());
-//    }
-//
-//    @Test
-//    @DisplayName("Request 의 start-line 에 포함된 URL을 파싱해서 리턴한다.")
-//    void parseUrl() {
-//        String startLineChunk = "GET /user/create?userId=hyun&password=1234&name=%ED%99%A9%ED%98%84&email=ghkdgus29%40naver.com HTTP/1.1";
-//
-//        StartLine startLine = HttpRequest.getStartLine(startLineChunk);
-//
-//        assertEquals( PATH + "/user/create", startLine.getPath());
-//    }
-//
-//    @Test
-//    @DisplayName("Request 의 start-line 에 포함된 URL에 파라미터가 없더라도 URL을 파싱해서 리턴한다.")
-//    void parseUrlWithoutParam() {
-//        String startLineChunk = "GET /index.html HTTP/1.1";
-//
-//        StartLine startLine = HttpRequest.getStartLine(startLineChunk);
-//
-//        assertEquals(PATH + "/index.html", startLine.getPath());
-//    }
-//
-//    @Test
-//    @DisplayName("Request 의 start-line 에 포함된 URL에서 파라미터를 뽑아내 paramMap으로 반환한다.")
-//    void parseParams() {
-//        String startLineChunk = "GET /user/create?userId=hyun&password=1234&name=%ED%99%A9%ED%98%84&email=ghkdgus29%40naver.com HTTP/1.1";
-//
-//        Map<String, String> expectedParams = Map.of("userId", "hyun",
-//                "password", "1234",
-//                "name", "%ED%99%A9%ED%98%84",
-//                "email", "ghkdgus29%40naver.com");
-//
-//        StartLine startLine = HttpRequest.getStartLine(startLineChunk);
-//
-//        assertEquals(expectedParams, startLine.getParamMap());
-//    }
-//
-//    @Test
-//    @DisplayName("Request 의 start-line 에 포함된 URL에 파라미터가 없으면 paramMap으로 null을 반환한다.")
-//    void parseNoParams() {
-//        String startLineChunk = "GET /user/create HTTP/1.1";
-//
-//        StartLine startLine = HttpRequest.getStartLine(startLineChunk);
-//
-//        assertNull(startLine.getParamMap());
-//    }
+    @Test
+    @DisplayName("HttpRequest 객체는 request-line 에 포함된 GET 메서드를 파싱해서 반환한다.")
+    void parseMethod() {
+
+        String requestLine = "GET /index.html HTTP/1.1";
+        Map<String, String> headers = Map.of("Host", "localhost:8080"
+                , "Connection", "keep-alive");
+
+        HttpRequest httpRequest = new HttpRequest(requestLine, headers, null);
+
+        assertEquals("GET", httpRequest.getMethod());
+    }
+
+    @Test
+    @DisplayName("HttpRequest 객체는 Request가 요청한 자료의 절대 경로를 반환한다.")
+    void parseUrl() {
+        String requestLine = "GET /user/create?userId=hyun&password=1234&name=%ED%99%A9%ED%98%84&email=ghkdgus29%40naver.com HTTP/1.1";
+        Map<String, String> headers = Map.of("Host", "localhost:8080"
+                , "Connection", "keep-alive");
+
+        HttpRequest httpRequest = new HttpRequest(requestLine, headers, null);
+
+        assertEquals(PATH + "/user/create", httpRequest.getPath());
+    }
+
+    @Test
+    @DisplayName("HttpRequest 객체는 request-line 에 포함된 URL에 파라미터가 없더라도 자료의 절대 경로를 반환한다.")
+    void parseUrlWithoutParam() {
+        String requestLine = "GET /index.html HTTP/1.1";
+        Map<String, String> headers = Map.of("Host", "localhost:8080"
+                , "Connection", "keep-alive");
+
+        HttpRequest httpRequest = new HttpRequest(requestLine, headers, null);
+
+        assertEquals(PATH + "/index.html", httpRequest.getPath());
+    }
+
+    @Test
+    @DisplayName("HttpRequest 객체는 headers 를 맵으로 반환한다.")
+    void parseHeaders() {
+        String requestLine = "GET /index.html HTTP/1.1";
+        Map<String, String> headers = Map.of("Host", "localhost:8080"
+                , "Connection", "keep-alive");
+
+        HttpRequest httpRequest = new HttpRequest(requestLine, headers, null);
+
+        assertEquals(headers, httpRequest.getHeaders());
+    }
+
+    @Test
+    @DisplayName("GET 요청의 경우, HttpRequest 객체는 request-line 에 포함된 URL에서 파라미터를 뽑아내 parameters 맵으로 반환한다.")
+    void parseParams() {
+        String requestLine = "GET /user/create?userId=hyun&password=1234&name=%ED%99%A9%ED%98%84&email=ghkdgus29%40naver.com HTTP/1.1";
+        Map<String, String> headers = Map.of("Host", "localhost:8080"
+                , "Connection", "keep-alive");
+
+        Map<String, String> expectedParams = Map.of("userId", "hyun",
+                "password", "1234",
+                "name", "%ED%99%A9%ED%98%84",
+                "email", "ghkdgus29%40naver.com");
+
+        HttpRequest httpRequest = new HttpRequest(requestLine, headers, null);
+
+        assertEquals(expectedParams, httpRequest.getParameters());
+    }
+
+    @Test
+    @DisplayName("GET 요청의 경우, HttpRequest 객체는 request-line 에 포함된 URL에 파라미터가 없으면 parameters 로 null을 반환한다.")
+    void parseNoParams() {
+        String requestLine = "GET /user/create HTTP/1.1";
+        Map<String, String> headers = Map.of("Host", "localhost:8080"
+                , "Connection", "keep-alive");
+
+        HttpRequest httpRequest = new HttpRequest(requestLine, headers, null);
+
+        assertNull(httpRequest.getParameters());
+    }
+
+    @Test
+    @DisplayName("POST 요청의 경우, HttpRequest 객체는 message body 에서 파라미터를 뽑아내 parameters 맵으로 반환한다.")
+    void parseParametersBody() {
+        String requestLine = "POST /user/create HTTP/1.1";
+        Map<String, String> headers = Map.of("Host", "localhost:8080"
+                , "Connection", "keep-alive");
+        String messageBody = "userId=hyun&password=1234&name=hyun&email=123%40123";
+
+        HttpRequest httpRequest = new HttpRequest(requestLine, headers, messageBody);
+
+        Map<String, String> expectedParameters = Map.of("userId", "hyun",
+                "password", "1234",
+                "name", "hyun",
+                "email", "123%40123");
+
+        assertEquals(expectedParameters, httpRequest.getParameters());
+    }
+
+    @Test
+    @DisplayName("POST 요청이지만 message body 가 없는 경우에는 parameters 는 null 이다.")
+    void parseParameterNoBody() {
+        String requestLine = "POST /user/create HTTP/1.1";
+        Map<String, String> headers = Map.of("Host", "localhost:8080"
+                , "Connection", "keep-alive");
+
+        HttpRequest httpRequest = new HttpRequest(requestLine, headers, null);
+
+        assertNull(httpRequest.getParameters());
+    }
 }
