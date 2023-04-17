b773e2ffd72c602bb4493f99a0bf7064ae81cb5e
Hyun <ghkdgus29@naver.com>
2023-04-17 15:13:46 +0900

    feat: HttpResponse 객체 내에 messageBody 를 읽어오는 getFile 메서드 추가
    getFile 메서드는 현재 요청이 HTML인 경우에만 FileReader 를 사용하여 HTML 파일을 한 줄씩 읽어온다.
    이는 한 줄씩 검사한 뒤 원하는 위치에 동적으로 HTML 을 넣어주기 위함이다.
    
    HTML 이외의 요청은 이전과 똑같이 Files.readAllBytes 사용

--- src/main/java/webserver/HttpResponse.java
+++ src/main/java/webserver/HttpResponse.java
@@ -3,7 +3,9 @@ package webserver;
 import model.RequestType;
 import util.StatusCode;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.HashMap;
@@ -45,7 +47,8 @@ public class HttpResponse {
             return;
         }
 
-        this.messageBody = Files.readAllBytes(new File(requestType.getAbsolutePath(viewName)).toPath());
+        this.messageBody = getFile(viewName);
+
         addHeader("Content-Type", requestType.getContentType());
         addHeader("Content-Length", String.valueOf(messageBody.length));
     }
@@ -70,10 +73,34 @@ public class HttpResponse {
         return concatHeadersAndBody(headers, messageBody);
     }
 
-    private static byte[] concatHeadersAndBody(byte[] headers, byte[] messageBody) {
+    private byte[] concatHeadersAndBody(byte[] headers, byte[] messageBody) {
         byte[] result = new byte[headers.length + messageBody.length];
         System.arraycopy(headers, 0, result, 0, headers.length);
         System.arraycopy(messageBody, 0, result, headers.length, messageBody.length);
         return result;
+    }
+
+    /**
+     * HTML 인 경우, 한 줄씩 읽기 위해 BufferedReader, FileReader 를 사용해서 파일 정보를 불러온다.
+     * (동적 HTML을 작성하기 위해)
+     * HTML 이 아닌 경우, 문자열이 아닐 수 있고, 한 줄씩 읽어올 필요도 없으므로 readAllBytes 를 사용한다.
+     *
+     * @param viewName
+     * @return
+     */
+    private byte[] getFile(String viewName) throws IOException {
+        if (requestType == RequestType.HTML) {
+            try (FileReader fr = new FileReader(requestType.getAbsolutePath(viewName));
+                 BufferedReader br = new BufferedReader(fr)) {
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line + "\r\n");
+                }
+                return sb.toString().getBytes();
+            }
+        }
+        return Files.readAllBytes(new File(requestType.getAbsolutePath(viewName)).toPath());
     }
 }
